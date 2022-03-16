46108bd56b44d20929c697e6feb6fb40f9ace3f9
Victor Eduardo Garcia Sardaneta <regulusleoc@gmail.com>
2022-03-16 08:37:01 -0600

    New

--- dtslib.py
+++ dtslib.py
@@ -1,6 +1,5 @@
 def counting_sort(list, unidad):
     size = len(list)
-    print(f"Lista {list}")
     # Crea una lista de apoyo hasta el indice 9
     nlist = [0] * 10
 
@@ -12,56 +11,23 @@ def counting_sort(list, unidad):
         val = list[i] // unidad
         nlist[int(val % 10)] += 1
 
+    # Se realiza una sumatoria
     for i in range(1, 10):
         nlist[i] = nlist[i] + nlist[i - 1]
 
 
-    # i = size - 1
-    # while i >= 1:
-    #     nlist[i] = nlist[i - 1]
-    #     i = i - 1
-
-    # for i in range(size-1, 0, -1):
-    #     val = list[i] // unidad
-    #     nlist[int(val % 10)] = nlist[int(val % 10) - 1]
-
-    # nlist[0] = 0
-    # print(nlist)
-
-    # for i in range(size - 1, 0, -1):
-    #     val = list[i] // unidad
-    #     aux[nlist[int(val % 10)] - 1] = list[i]
-    #     nlist[int(val % 10)] = nlist[int(val % 10)] + 1
-    i = 0
-    while i < size:
+    i = size - 1
+    while i >= 0:
         val = list[i] // unidad
-        print(f"{aux}")
-        print(nlist[val % 10]-1)
         aux[nlist[val % 10] - 1] = list[i]
         nlist[val % 10] -= 1
-        i += 1
+        i -= 1
 
-    # for i in range(0, size):
-    #     for f in range(0, size):
-    #         val = list[i] // unidad
-    #         if list[i] == f:
-    #             aux[nlist[f]] = list[i]
-    #             nlist[f] = nlist[f] + 1
+    # print(f"{aux}")
 
-    # # for i in range(0, size):
-    #     val = list[i] // unidad
-    #     print(nlist[val % 10] - 1)
-    #     print(f"{aux}")
-    #     aux[nlist[val % 10] - 1] = list[i]
-    #     nlist[val % 10] += 1
-
-    # print(f"{nlist}")
-    print(f"{aux}")
-
+    #Se transfiere el arreglo ordenada de aux a list, actualizando list en caso que se necesite otra iteración
     for i in range(0, len(list)):
         list[i] = aux[i]
-
-
 
 
 def radix_sort(list):
@@ -76,3 +42,5 @@ def radix_sort(list):
     while max / unidad > 1:
         counting_sort(list, unidad)
         unidad = unidad * 10
+
+    return list
